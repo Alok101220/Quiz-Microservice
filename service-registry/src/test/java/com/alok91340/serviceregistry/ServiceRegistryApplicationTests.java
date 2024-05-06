994800cd6a942bd7bca2c0ba5e44e493e82b87bc
Alok Singh <alok91340@gmail.com>
2024-05-06 18:00:54 +0530

    uploaded service-registry

---
+++ service-registry/src/test/java/com/alok91340/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alok91340.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
